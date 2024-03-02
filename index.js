c74277698dc3b86320bacbf62ca5126c1256f201
DanielaGolubov <danielagincu2016@gmail.com>
2024-03-02 22:43:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// // ex1
+// let teststring = 'lala'
+// console.log(teststring)
+
+// let testnumber = 8
+// console.log(testnumber)
+
+// var testboolean = true
+// console.log(testboolean)
+
+// const testundefiend = undefined
+// console.log(testundefiend)
+
+// let testnull = null
+// console.log(testnull)
+
+// let testbigint = BigInt(900)
+// console.log(testbigint)
+
+// let testsymbol = Symbol('')
+// console.log(testsymbol)
+
+// let testobject = {
+//     color: 'green'}
+// console.log(testobject)
+
+// // ex2
+// console.log(typeof teststring)
+// console.log(typeof testnumber)
+// console.log(typeof testboolean)
+// console.log(typeof testundefiend)
+// console.log(typeof testnull)
+// console.log(typeof testbigint)
+// console.log(typeof testsymbol)
+// console.log(typeof testobject)
+
+//ex3 (+, ++, -, --, /, *, **, >, <, >=, <=, !, ==, ===)
+
+let test1 = 9
+let test2 = 6
+let test3 = 3
+let test4 = false
+let test5 = 6
+console.log(test1 + test2) //15
+console.log(test1 - test2) //3
+console.log(++test2) //7
+console.log(test1++) //9
+console.log(test2 - test1) //-3
+console.log(--test2) //6
+console.log(test2--) //6
+console.log(!test4) //true
+console.log(test1 / test3) //3.3333333333333335
+console.log(test3 * test2) //15
+console.log(test1 > test3) //true
+console.log(test5 < test1) //true
+console.log(test2 >= test3) //true
+console.log(test2 <= test1) //true
+console.log(test2 == test5) //true
+console.log(test5 === test4) //false
+console.log(test2 === test5) //true
+console.log(test5 ** test3) //216
+
+
+
+
